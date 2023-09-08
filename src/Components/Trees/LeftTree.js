cd8f66a8871144c5a8459c8e82982e7c00ff2a21
Taranveer Singh <taranveersingh93@gmail.com>
2023-09-08 12:40:23 -0400

    Animate m1

--- src/Components/Trees/LeftTree.js
+++ src/Components/Trees/LeftTree.js
@@ -7,7 +7,8 @@ const LeftTree = ({scrollValue}) => {
   const [treeTop, setTreeTop] = useState(30)
   const [sizeMultiplier, setSizeMultiplier] = useState(1);
   useEffect(() => {
-    // console.log(scrollValue)
+    setTreeLeft(-15 - (0.18*scrollValue))
+    setSizeMultiplier(1 + (scrollValue*0.007))
   }, [scrollValue])
   return (
     <img className='left-trees' src={leftTreesImg} style={{top: `${treeTop}vh`, left: `${treeLeft}vw`, transform: `scale(${sizeMultiplier})`}}/>
