dcd6241380107ed3fe638b3364be1c9b8e5d31c4
Taranveer Singh <taranveersingh93@gmail.com>
2023-09-08 21:16:26 -0400

    Resize text

--- src/Components/Texts/Win4.js
+++ src/Components/Texts/Win4.js
@@ -2,7 +2,7 @@ import './texts.css'
 
 const Win4 = ({data, scrollValue}) => {
   return (
-    <p className='win1-text' style={{top: `${-20 + 0.017*scrollValue}vh`}}>
+    <p className='win4-text' style={{top: `${-15 + 0.017*scrollValue}vh`}}>
       {data?.win4}
     </p>
   )
