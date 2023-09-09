6dbf211456eac68d2b8d649114d23f71e0971b67
Taranveer Singh <taranveersingh93@gmail.com>
2023-09-08 21:23:25 -0400

    Replace assurance text

--- src/Components/Texts/Assurance2.js
+++ src/Components/Texts/Assurance2.js
@@ -3,7 +3,7 @@ import './texts.css'
 const Assurance2 = ({scrollValue}) => {
   return (
     <p className='assurance-text' style={{top: `${40 - 0.001*scrollValue}vh`, left: `${20 - 0.0005 * scrollValue}vw`}}>
-      But so were the previous ones.
+      But so were the previous obstacles.
     </p>
   )
 }
